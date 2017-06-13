f98a18637a29e204f16b98c30145d0344ce261c2
dstaatz <dstaatz@ksu.edu>
2017-06-13 09:51:32 -0500

    Created both the Blogpost and comment model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone()
+
 
 # Create your models here.
+class Blogpost(models.Model):
+    blog_title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.blog_title
+
+    
+class Comment(models.Model):
+    blogpost = models.ForeignKey(Blogpost, on_delete=models.CASCADE)
+    content = models.CharField(max_length=400)
+    author = models.CharField(max_length=30)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.content
